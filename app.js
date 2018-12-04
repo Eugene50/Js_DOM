b44b5a212af216670e981075f08c3cae32f0e6a5
unknown <nova_alf@ukr.net>
2018-12-04 11:52:17 +0200

    change some functions and add new variable

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 window.onload = function () {
-     const cont  = document.getElementById("container");
-   
-   const x = document.createElement("h1");
-x.textContent = 'test test';
+  const container  = document.getElementById("container");
+  var content = document.getElementById("content");
+
+
+const x = document.createElement("h1");
+x.textContent = 'Oboe World';
 document.body.insertBefore(x, document.body.firstChild);
 
 data.forEach(function (index) {
 
-     const div = document.createElement("p");
-   div.innerHTML = index.name;
-   cont.appendChild(div);
+  const div = document.createElement("p");
+div.innerHTML = index.name;
+container.appendChild(div);
 });
 
-cont.addEventListener('click', function (el) {
-  //const curentOboe = [];
-  if(el.target.tagName != 'p') 
-  return 
-   const curentOboe = data.find(function (ind) {
-       ind.name === el.target.textContent
-       cont.innerHTML = curentOboe.text;
-   })
+container.addEventListener('click', function (el) {
 
+if(el.target.tagName != 'p') {
+ var currentOboe = data.find(index => index.name === el.target.textContent)
+}
+content.innerHTML = currentOboe.text + `<img src = "${currentOboe.img}" class = "img">`; 
 })
 
+//  container.addEventListener("click", function () {
+//  div.classList.toggle('visible');
+
+// })
+
 };
+
