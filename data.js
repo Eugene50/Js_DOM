1c15c794efff4ec1b3148822d5de051cf4c07f75
unknown <nova_alf@ukr.net>
2018-12-06 17:53:37 +0200

    rename values

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var data = [
    {
-      name: 'Marigaux',
+      id: 'Marigaux',
  
       text: 'Personalised instrument Individual choice of head joints Streamline design Improved tuning 3 head-joint lenghts available : short, standard and long',
  
@@ -8,7 +8,7 @@ var data = [
     },
  
     {
-       name: 'Rigoutat',
+       id: 'Rigoutat',
  
        text: 'Premiere French Oboe maker since 1922 RIGOUTAT makes high-level professional oboes. Made of African Grenadilla wood or of violet wood, they are available in the Classical, Symphony and Evolution bore design (Violet wood available only in Expression and J models). By the richness of its tone quality, the Classical RIGOUTAT oboe remains the oboe of soloists and concert artists.',
  
@@ -16,7 +16,7 @@ var data = [
     },
     {
        
-    name: 'Loree',
+    id: 'Loree',
  
     text: 'Fondée en 1881, F.Lorée est la plus ancienne maison spécialisée dans la fabrication de hautbois.Conjuguant savoir-faire et traditions de fabrication, la firme, actuellement dirigée par Alain de Gourdon, ne cesse d’innover et d’améliorer ses techniques de production pour répondre aux besoins des musiciens de toutes nationalités.',
  
@@ -25,7 +25,7 @@ var data = [
     },
  
     {
-       name: 'Dupin',
+       id: 'Dupin',
  
        text: 'Further to the success gained by this instrument - that the oboists describe as being a revolution in the world of the oboe - some Japanese and European manufacturers tried to copy Impérial Dupin without however managing ever to equal the quality of the sound and the general quality of the instrument.',
  
